1dfeb6cf75baacc5b0ff1af9ad70149d8b0e2acd
AgusGol <agustingolberg@gmail.com>
2022-10-22 20:04:37 +0200

    Agregando mas rutas en en app.js

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ app.listen(3031, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
 })
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"))
+})
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productdetail.html"))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
