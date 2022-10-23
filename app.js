8869b2ecd5f64594260a19f1aec879944291e7fe
AgusGol <agustingolberg@gmail.com>
2022-10-23 20:33:29 +0200

    creacion del styles.css, armado de navbar y footer reutilizables (falta q quede bien centrado en distintas resoluciones). Creacion de la home version mobile y adaptada a tables. falta desktop.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 //para decirle al server en que carpeta estan ubicados los elementos estaticos (fotos y style)
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3031, () => {
     console.log('Servidor arriba en el puerto 3031');
@@ -11,24 +11,24 @@ app.listen(3031, () => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
-})
+});
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
-})
+});
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
-})
+});
 
 app.get('/productCart', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/productCart.html"))
-})
+});
 
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/productdetail.html"))
-})
+});
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"))
-})
+});
