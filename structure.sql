ef8274c84df3ccb3093d500de9ee6f189f207b61
AgusGol <agustingolberg@gmail.com>
2023-01-17 20:59:48 +0100

    algunos cambios al sql basados en el feedback de gian

--- structure.sql
+++ structure.sql
@@ -1,35 +1,24 @@
 CREATE DATABASE `synthworld`;
 
-CREATE TABLE `synthworld`.`user_categories`
+
+
+
+
+CREATE TABLE `Synthworld`.`users`
 (
- `id`   INT NOT NULL AUTO_INCREMENT ,
- `name` VARCHAR(45) NOT NULL ,
+ `id`        INT NOT NULL AUTO_INCREMENT ,
+ `name`      VARCHAR(45) NOT NULL ,
+ `last_name` VARCHAR(45) NULL ,
+ `email`     VARCHAR(45) NOT NULL ,
+ `password`  VARCHAR(100) NOT NULL ,
+ `image`     VARCHAR(100) NULL ,
+ `isAdmin`   BINARY NOT NULL ,
 
 PRIMARY KEY (`id`)
 );
 
 
-
-CREATE TABLE `synthworld`.`users`
-(
- `id`               INT NOT NULL AUTO_INCREMENT ,
- `name`             VARCHAR(45) NOT NULL ,
- `user_category_id` INT NOT NULL ,
- `last_name`        VARCHAR(45) NULL ,
- `email`            VARCHAR(45) NOT NULL ,
- `password`         VARCHAR(100) NOT NULL ,
- `image`            VARCHAR(100) NULL ,
-
-PRIMARY KEY (`id`),
-KEY `user_category` (`user_category_id`),
-CONSTRAINT `user_category` FOREIGN KEY `user_category` (`user_category_id`) REFERENCES `Synthworld`.`user_categories` (`id`)
-);
-
-
-
-
-
-CREATE TABLE `synthworld`.`product_categories`
+CREATE TABLE `synthworld`.`categories`
 (
  `id`   int NOT NULL AUTO_INCREMENT ,
  `name` varchar(45) NOT NULL ,
@@ -57,7 +46,7 @@ CREATE TABLE `synthworld`.`products`
  `id`                  INT NOT NULL AUTO_INCREMENT ,
  `name`                VARCHAR(100) NOT NULL ,
  `brand_id`            INT NULL ,
- `product_category_id` INT NOT NULL ,
+ `category_id`         INT NOT NULL ,
  `price`               DECIMAL(10, 2) NOT NULL ,
  `discount`            decimal(4, 2) NULL ,
  `image`               VARCHAR(100) NOT NULL ,
@@ -66,8 +55,8 @@ CREATE TABLE `synthworld`.`products`
  `availability`        BINARY NOT NULL,
 
 PRIMARY KEY (`id`),
-KEY `product_category` (`product_category_id`),
-CONSTRAINT `product_category` FOREIGN KEY `product_category` (`product_category_id`) REFERENCES `Synthworld`.`product_categories` (`id`),
+KEY `category` (`category_id`),
+CONSTRAINT `category` FOREIGN KEY `category` (`category_id`) REFERENCES `Synthworld`.`categories` (`id`),
 KEY `brand` (`brand_id`),
 CONSTRAINT `brand` FOREIGN KEY `brand` (`brand_id`) REFERENCES `Synthworld`.`brands` (`id`)
 );
@@ -105,8 +94,8 @@ CREATE TABLE `synthworld`.`order_products`
 PRIMARY KEY (`id`),
 KEY `product` (`product_id`),
 CONSTRAINT `product` FOREIGN KEY `product` (`product_id`) REFERENCES `Synthworld`.`products` (`id`),
-KEY `FK_3` (`order_id`),
-CONSTRAINT `FK_6` FOREIGN KEY `FK_3` (`order_id`) REFERENCES `Synthworld`.`orders` (`id`)
+KEY `order` (`order_id`),
+CONSTRAINT `order` FOREIGN KEY `order` (`order_id`) REFERENCES `Synthworld`.`orders` (`id`)
 );
 
 
