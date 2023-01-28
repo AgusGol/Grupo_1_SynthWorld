4964fb58aa2bcab7c8c9028c33a342e89aef3dac
MatiPechu <matiasrobertone87@gmail.com>
2023-01-29 00:11:01 +0100

    base con script como ejemplo

---
+++ script_ejemplo/categories.sql
@@ -0,0 +1,7 @@
+insert into categories (id, name, created_at, updated_at) values (1, 'Services', null, null);
+insert into categories (id, name, created_at, updated_at) values (2, 'Accounting', null, null);
+insert into categories (id, name, created_at, updated_at) values (3, 'Engineering', null, null);
+insert into categories (id, name, created_at, updated_at) values (4, 'Human Resources', null, null);
+insert into categories (id, name, created_at, updated_at) values (5, 'Marketing', null, null);
+insert into categories (id, name, created_at, updated_at) values (6, 'Legal', null, null);
+insert into categories (id, name, created_at, updated_at) values (7, 'Human Resources', null, null);
