301e6253072eb242b20aae226894d44ccd008066
AgusGol <agustingolberg@gmail.com>
2023-01-29 20:35:08 +0100

    modificando losn nombres de las categoris en el script ejemplo y borrando el id xq se asigna solo

--- script_ejemplo/categories.sql
+++ script_ejemplo/categories.sql
@@ -1,7 +1,5 @@
-insert into categories (id, name, created_at, updated_at) values (1, 'Services', null, null);
-insert into categories (id, name, created_at, updated_at) values (2, 'Accounting', null, null);
-insert into categories (id, name, created_at, updated_at) values (3, 'Engineering', null, null);
-insert into categories (id, name, created_at, updated_at) values (4, 'Human Resources', null, null);
-insert into categories (id, name, created_at, updated_at) values (5, 'Marketing', null, null);
-insert into categories (id, name, created_at, updated_at) values (6, 'Legal', null, null);
-insert into categories (id, name, created_at, updated_at) values (7, 'Human Resources', null, null);
+insert into categories (name) values ('modules_and_cases');
+insert into categories (name) values ('Sequencers');
+insert into categories (name) values ('Effects');
+insert into categories (name) values ('Accesories');
+insert into categories (name) values ('Weird Gear');
