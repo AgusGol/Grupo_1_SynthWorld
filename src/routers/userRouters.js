3b80ac65a350285ef875e288afb4e31e4bd47ccc
MatiPechu <matiasrobertone87@gmail.com>
2023-02-03 01:49:36 +0100

    validaciones express Fron, falta analaizar algun punto

--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -9,13 +9,19 @@ const loggedMiddleware = require('../middlewares/loggedMiddleware');
 
 const validateLogin = [
     check('userEmail')
-    .notEmpty().withMessage('You must fill in your email address').bail()
+    .notEmpty().withMessage('You must fill in your email address').bail().exists()
     .isEmail().withMessage('You must fill in a valid email address'),
     check('userPassword')
     .notEmpty().withMessage('You must fill in your password').bail(),
     ];
 //validacion del registe  aca o en un apartado de middleware?    
 const validateRegister = [
+    check('name')
+        .notEmpty()
+        .isLength({minLength: 2}),
+    check('last_name')
+        .notEmpty()
+        .isLength({minLength: 2}),    
     check('email')
         .notEmpty()
         .withMessage('You must fill in your email address').bail()
@@ -28,7 +34,14 @@ const validateRegister = [
         .bail(),
     check('password2')
         .notEmpty()
-        .custom()
+        .custom(),
+    check('userAvatar')
+        .custom(({req}) => {
+        if(req.file.mimetype === 'image/jpeg'|| req.file.mimetype === 'image/gif' || req.file.mimetype === 'image/png'){
+            return true;}
+        else{
+            return false;}})
+        .withMessage('Must be a valid file (JPG, JPEG, PNG, GIF).')
     ];
 
 // Multer //
