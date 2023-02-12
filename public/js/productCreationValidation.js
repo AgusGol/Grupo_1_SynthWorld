a173331b889a9f6682b2216b2db60b03364a44e3
Vicky <mvictoriaripa@gmail.com>
2023-02-12 21:06:14 +0100

    Creación de productCreationValidation en public/js. Captura de campos de html y agregado de id en form de productCreation para trabajarlos en javascript

---
+++ public/js/productCreationValidation.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+    let form = document.querySelector('form');
+    let productName = document.querySelector('#productName')
+    let productDescription = document.querySelector('#description')
+    let productExtraInfo = document.querySelector('#extraInfo')
+    let productPrice = document.querySelector('#price')
+    let productDiscount = document.querySelector('#discount')
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+})
+
+
+console.log(form)
+console.log(productName)
+console.log(productDescription)
+console.log(productExtraInfo)
+console.log(productPrice)
+console.log(productDiscount)
+
+
+
+
+
+
+
+
+})
