2a698ed554a93b6afbd27d2f7f6538a45628bbdd
MatiPechu <matiasrobertone87@gmail.com>
2023-02-13 22:17:54 +0100

    Validaciones de Front Vinculadas y product create funcionando de nuevo!

--- public/js/productCreationValidation.js
+++ public/js/productCreationValidation.js
@@ -4,42 +4,48 @@ window.addEventListener('load', () => {
     let productDescription = document.querySelector('#description')
     let productPrice = document.querySelector('#price')
 
-    form.addEventListener("submit", function (e) {
-        if(!validaciones(e)){
-            e.preventDefault();
+    form.addEventListener("submit", function (evento) {
+        if(!validaciones(evento)){
+            evento.preventDefault();
         } else{
             form.submit();
         }
+        function validaciones (evento) {
 
     //falta acomodar el submit. No me queda claro un detalle 
-function validaciones (e) {
     let errores = []; 
-if (productName == " ") {
-    errores.push("You must define a product name")
+if (productName.value == "") {
+    errores.push("You must define a product name");
 } else if (productName.value.length < 6) {
-    errores.push("The product name must be at least 5 characters long")
-}
+    errores.push("The product name must be at least 5 characters long");
+} else{
+    errores.push("")
+} 
 
-if (productDescription == " ") {
-    errores.push("You must define a product description")
+if (productDescription.value == "") {
+    errores.push("You must define a product description");
 } else if (productName.value.length < 21) {
-    errores.push("The product description must be at least 21 characters long")
+    errores.push("The product description must be at least 21 characters long");
+} else{
+    errores.push("")
 }
 
-if (productPrice == " ") {
+if (productPrice.value == "") {
     errores.push("You must define a product price")
 } else if (productPrice.value > 0) {
     errores.push("The product price must be greater than 0")
+} else{
+    errores.push("")
 }
 
 
     //pendiente: revisar estas líneas para mostrarlas como listado. Modificar el html
-let ulErrores = document.querySelector("div.errores");
-if(errores.values>0){
-    e.preventDefault();
+let ulErrores = document.getElementById('errores');
+if(errores.length > 0){
+    evento.preventDefault();
     ulErrores.innerHTML="";
 for (let i = 0; i < errores.length; i++) {
-    ulErrores.innerHTML += `<li> ${errores[i]} </li>`}
+    ulErrores.innerHTML += `<li> ${errores[i]} </li> `}
     errores = [];    
 } else {
     return true;
@@ -59,12 +65,4 @@ console.log(errores)
 // console.log(productExtraInfo)
 // console.log(productPrice)
 // console.log(productDiscount)
-
-
-
-
-
-
-
-
 })
