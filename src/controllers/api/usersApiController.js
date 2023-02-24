93fb225d9106facf94f3b2e74485f7e87098e33b
MatiPechu <matiasrobertone87@gmail.com>
2023-02-24 02:04:20 +0100

    Api Users biggin

---
+++ src/controllers/api/usersApiController.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const db = require('../../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+
+
+//Aqui tienen otra forma de llamar a cada uno de los modelos
+const Users = db.User;
+
+
+
+const usersApiControllerr = {
+    'list': (req, res) => {
+        db.User.findAll()
+        .then(clientes => {
+            let respuesta = {
+                meta: {
+                    status: 200,
+                    count: clientes.length,
+                    url: '/api/clientes'
+                },
+                data: clientes.map(cliente=>{
+                    return{
+                        id:cliente.id,
+                        name:cliente.name,
+                        email:cliente.email,
+                        url:"/api/clientes/:"+ cliente.id,
+
+                    }
+                })
+            }
+                res.json(respuesta);
+            })
+    },
+        
+    'detail': (req, res) => {
+        db.User.findByPk(req.params.id)
+            .then(cliente => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        url: '/api/clientes/:id'
+                    },
+                    data: clientes.map(cliente=>{
+                        return{
+                            id:cliente.id,
+                            name:cliente.name,
+                            last_name:cliente.last_name,
+                            email:cliente.email,
+                            created_at:cliente.created_at,
+                            updated_at:cliente.updated_at,
+                            // image:"http://localhost:3031/api/clientes/:"+ cliente.id+cliente.image,
+    
+                        }
+                    })
+                }
+                res.json(respuesta);
+            });
+    },
+    
+    
+}
+
+module.exports = usersApiController;
